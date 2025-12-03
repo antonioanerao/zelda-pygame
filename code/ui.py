fe7b99f666c1912db5f6c359de69f13cf537b50f
Antonio Anerão <anerao.junior@gmail.com>
2025-12-03 04:41:48 -0500

    update: health and mana bars; add: exp bar

--- code/ui.py
+++ code/ui.py
@@ -13,8 +13,28 @@ class UI:
         self.energy_bar_rect = pygame.Rect(10, 34, settings.ENERGY_BAR_WIDTH, settings.BAR_HEIGHT)
 
     def show_bar(self, current, max_amount, bg_rect, color):
-        pygame.draw.rect(self.display_surface, color, bg_rect)
+        pygame.draw.rect(self.display_surface, settings.UI_BG_COLOR, bg_rect)
+
+        # Stat to pixel
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        pygame.draw.rect(self.display_surface, color, current_rect)
+        pygame.draw.rect(self.display_surface, settings.UI_BORDER_COLOR, bg_rect, 3)
+
+    def show_exp(self, exp):
+        text_surf = self.font.render(str(int(exp)), False, settings.TEXT_COLOR)
+        x = self.display_surface.get_size()[0] - 20
+        y = self.display_surface.get_size()[1] - 20
+        text_rect = text_surf.get_rect(bottomright=(x, y))
+
+        pygame.draw.rect(self.display_surface, settings.UI_BG_COLOR, text_rect.inflate(20, 20))
+        self.display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(self.display_surface, settings.UI_BORDER_COLOR, text_rect.inflate(20, 20), 1)
 
     def display(self, player):
         self.show_bar(player.health, player.stats['health'], self.health_bar_rect, settings.HEALTH_COLOR)
         self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, settings.ENERGY_COLOR)
+        self.show_exp(player.exp)
